5218699f673b3f46aad53ac517bb1372605936d5
chengqing <21251189@zju.edu.cn>
2014-04-17 20:29:45 +0800

    add basic Interval

---
+++ PFWhat/src/leecode/basic/Interval.java
@@ -0,0 +1,20 @@
+package leecode.basic;
+
+/**
+ * Definition for an interval.
+ * 
+ */
+public class Interval {
+	int start;
+	int end;
+
+	Interval() {
+		start = 0;
+		end = 0;
+	}
+
+	Interval(int s, int e) {
+		start = s;
+		end = e;
+	}
+}
